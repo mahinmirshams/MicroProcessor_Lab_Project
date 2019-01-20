25a78ada7701974ced35959095681462170bc5dc
Alireza Heydari <alireza97hi@gmail.com>
2019-01-20 11:08:57 +0330

    my_init started

--- note.c
+++ note.c
@@ -7,8 +7,15 @@ unsigned int fn_data[24] = {440, 466, 494, 523, 554, 587, 622, 659, 698, 740,
                             784, 831, 880, 932, 988, 1047, 1109, 1175, 1245,
                             1319, 1397, 1480, 1568, 1661};
 
-
 void main(void)
 {
+
+}
+
+void my_init(void)
+{
+    DDRA = (0 << DDA7) | (0 << DDA6) | (0 << DDA5) | (0 << DDA4) | (0 << DDA3) | (0 << DDA2) | (0 << DDA1) | (0 << DDA0);
+    
+    PORTA = (0 << PORTA7) | (0 << PORTA6) | (0 << PORTA5) | (0 << PORTA4) | (0 << PORTA3) | (0 << PORTA2) | (0 << PORTA1) | (0 << PORTA0);
 
 }
